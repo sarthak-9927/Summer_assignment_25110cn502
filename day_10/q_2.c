55a433c344df5d970b2caf8d36397a89f8b0a438
sarthak gupta <sarthak4589@gmail.com>
2026-06-18 16:59:49 +0530

    day_10 assignment is uploaded

---
+++ day_10/q_2.c
@@ -0,0 +1,18 @@
+//program to print reverse star pryamid pattern
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
