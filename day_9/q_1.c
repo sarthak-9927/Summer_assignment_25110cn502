9cbc422c37b46c1b3ce97c2b1b39741fe2e4d52f
sarthak gupta <sarthak4589@gmail.com>
2026-06-18 16:58:20 +0530

    day_9 assignment is uploaded

---
+++ day_9/q_1.c
@@ -0,0 +1,15 @@
+//program to print reverse of half pyramid pattern
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
